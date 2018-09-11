266ac94a0e19715d8441b3a555fee600ef2c1314
Aaron Nous <aaronnous@Aarons-MacBook-Pro.local>
2018-09-11 16:13:51 +0300

    $ first commit;

---
+++ read_access_log.php
@@ -0,0 +1,13 @@
+<?php
+
+use Predis\Client;
+
+$config = [
+    'redis' => [
+        'scheme' => 'tcp',
+        'host' => '127.0.0.1',
+        'port' => 6379,
+        'password' => 'password'
+    ]
+];
+
